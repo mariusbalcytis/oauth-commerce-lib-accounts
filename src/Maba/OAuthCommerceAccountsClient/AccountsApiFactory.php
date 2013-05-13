0c86d7e745b3bf5ef90c0e1457c1a3912f5218b4
marius <marius.balcytis@gmail.com>
2013-05-13 21:09:20 +0300

    static method to load default container

--- src/Maba/OAuthCommerceAccountsClient/AccountsApiFactory.php
+++ src/Maba/OAuthCommerceAccountsClient/AccountsApiFactory.php
@@ -4,12 +4,16 @@
 namespace Maba\OAuthCommerceAccountsClient;
 
 use Guzzle\Service\Client;
+use Maba\OAuthCommerceAccountsClient\DependencyInjection\AccountsClientExtension;
 use Maba\OAuthCommerceClient\AuthClientFactory;
 use Maba\OAuthCommerceClient\CodeGrantHandler;
+use Maba\OAuthCommerceClient\DependencyInjection\BaseClientExtension;
 use Maba\OAuthCommerceClient\Entity\SignatureCredentials;
 use Maba\OAuthCommerceClient\MacSignature\AlgorithmManager;
 use Maba\OAuthCommerceClient\Plugin\MacSignatureProvider;
 use Maba\OAuthCommerceClient\Random\RandomProviderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 
 class AccountsApiFactory
 {
@@ -45,24 +49,38 @@ class AccountsApiFactory
             $signatureCredentials = $this->algorithmManager->createSignatureCredentials($signatureCredentials);
         }
 
-        if ($baseUrl !== null) {
-            $authUrl = isset($config['base_auth_url']) ? $config['base_auth_url'] : $baseUrl . '/api/auth/v1';
-            $accountsUrl = isset($config['base_accounts_url'])
-                ? $config['base_accounts_url']
-                : $baseUrl . '/api/accounts/v1';
-        } else {
-            $authUrl = null;
-            $accountsUrl = null;
-        }
+        $authUrl = isset($config['base_auth_url']) ? $config['base_auth_url'] : $baseUrl . '/api/auth/v1';
+        $accountsUrl = isset($config['base_accounts_url'])
+            ? $config['base_accounts_url']
+            : $baseUrl . '/api/accounts/v1';
         $oauthEndpointUrl = isset($config['oauth_endpoint'])
             ? $config['oauth_endpoint']
-            : ($baseUrl ? : self::DEFAULT_ENDPOINT) . '/confirm';
+            : $baseUrl . '/confirm';
 
         return new AccountsApi(
             $this->authFactory->createClient($signatureCredentials, $authUrl, $config),
             $this->accountsFactory->createClient($signatureCredentials, $accountsUrl, $config),
             new CodeGrantHandler($this->randomProvider, $signatureCredentials->getMacId(), $oauthEndpointUrl)
         );
+    }
+
+    public static function loadContainer(array $parameters = array(), $compile = true)
+    {
+        $container = new ContainerBuilder(new ParameterBag($parameters));
+        $container->setResourceTracking(false); // this is not needed if cache is not used, also it requires Config
+
+        $extension = new BaseClientExtension();
+        $container->registerExtension($extension);
+        $container->loadFromExtension($extension->getAlias());
+        $extension->addCompilerPasses($container);
+        $extension = new AccountsClientExtension();
+        $container->registerExtension($extension);
+        $container->loadFromExtension($extension->getAlias());
+
+        if ($compile) {
+            $container->compile();
+        }
+        return $container;
     }
 
 }
